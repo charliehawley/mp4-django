40bbc87bc8842298906e6192ae66133f70930d30
charliehawley <charlie.e.hawley@gmail.com>
2022-03-31 21:04:03 +0000

    Create initial model for "cracks" in wisecrack (not migrated), planning in readme

--- wisecrack/models.py
+++ wisecrack/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Crack(models.Model):
+    date = models.DateTimeField(null=False, blank=False)
+    crack = models.CharField(max_length=150, null=False, blank=False)
