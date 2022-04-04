ba431d81ef4bffdd98480beef3e351172009d5bc
charliehawley <charlie.e.hawley@gmail.com>
2022-04-04 14:51:06 +0000

    Add requirements.txt and configure first two models.

--- wisecrack/models.py
+++ wisecrack/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
 class Crack(models.Model):
     date = models.DateTimeField(null=False, blank=False)
-    crack = models.CharField(max_length=150, null=False, blank=False)
+    text = models.TextField(max_length=150, null=False, blank=False)
+
+    def __int__(self):
+        return self.date
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    password = models.CharField(max_length=30, null=False, blank=False)
+    top_subs = models.IntegerField(primary_key=False)
--- wisecrack/views.py
+++ wisecrack/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def go_home(request):
     return render(request, 'wisecrack/wisecrack.html')
