ab8ab292048965e8b3112918bcaff071a3f9e6a2
charliehawley <charlie.e.hawley@gmail.com>
2022-04-04 20:20:54 +0000

    Refactor models to include more functionality.

--- wisecrack/models.py
+++ wisecrack/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Crack(models.Model):
     date = models.DateTimeField(null=False, blank=False)
-    text = models.TextField(max_length=150, null=False, blank=False)
+    prompt = models.TextField(max_length=300, null=False, blank=False, unique=True, default="What's the crack?")
+    slug = models.SlugField(max_length=200)
 
     def __int__(self):
         return self.date
@@ -13,3 +16,19 @@ class User(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False)
     password = models.CharField(max_length=30, null=False, blank=False)
     top_subs = models.IntegerField(primary_key=False)
+    avatar = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Sub(models.Model):
+    sub = models.TextField(max_length=150, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    upvotes = models.IntegerField()
+
+    class Meta:
+        ordering = ['-upvotes']
+
+    def __str__(self):
+        return self.user.name
