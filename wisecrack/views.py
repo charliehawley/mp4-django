0f4f359eacbf4263d05aad5cc12129271d703203
charliehawley <charlie.e.hawley@gmail.com>
2022-04-10 15:23:00 +0000

    Remove submitted state. Submitted state no longer in scope.

--- wisecrack/views.py
+++ wisecrack/views.py
@@ -26,7 +26,6 @@ class UserSubList(View):
 
 
 class EditSub(View):
-
     def get(self, request, prompt, pk, *args, **kwargs):
         id = ''.join(x for x in prompt if x.isdigit())
         prompt_id = Prompt.objects.filter(pk=id)
@@ -89,7 +88,6 @@ class EditSub(View):
 class DeleteSub(View):
     def get(self, request, pk, sub, *args, **kwargs):
         subs = Sub.objects.filter(user=pk, sub=sub)
-        # del_sub = subs.get(prompt=prompt)
         subs.delete()
 
         # delete_sub = DeleteSub(data=request.DELETE)
@@ -126,58 +124,58 @@ class PromptDetail(View):
         queryset = Prompt.objects
         prompt = get_object_or_404(queryset, slug=slug)
         subs = Sub.objects.order_by('created_on')
-        submitted = False
-        if prompt.subs_list.filter(id=self.request.user.id).exists():
-            submitted = True
+        # submitted = False
+        # if prompt.subs_list.filter(id=self.request.user.id).exists():
+        #     submitted = True
 
         sub_form = SubForm(data=request.POST)
 
-        if submitted:
-            return render(
-                request,
-                "prompt_detail.html",
-                {
-                    "prompt": prompt,
-                    "subs": subs,
-                    "submitted": True,
-                    "sub_form": sub_form
-                }
-            )
+        # if submitted:
+            # return render(
+            #     request,
+            #     "prompt_detail.html",
+            #     {
+            #         "prompt": prompt,
+            #         "subs": subs,
+            #         # "submitted": True,
+            #         "sub_form": sub_form
+            #     }
+            # )
+        # else:
+        if sub_form.is_valid():
+            sub_form.instance.user = request.user
+            sub_form.instance.name = request.user.username
+            sub = sub_form.save(commit=False)
+            sub.prompt = prompt
+            sub.save()
         else:
-            if sub_form.is_valid():
-                sub_form.instance.user = request.user
-                sub_form.instance.name = request.user.username
-                sub = sub_form.save(commit=False)
-                sub.prompt = prompt
-                sub.save()
-            else:
-                sub_form = SubForm()
+            sub_form = SubForm()
 
-            return render(
-                request,
-                "prompt_detail.html",
-                {
-                    "prompt": prompt,
-                    "subs": subs,
-                    "submitted": False,
-                    "sub_form": sub_form
-                }
-            )
+        return render(
+            request,
+            "prompt_detail.html",
+            {
+                "prompt": prompt,
+                "subs": subs,
+                # "submitted": False,
+                "sub_form": sub_form
+            }
+        )
 
 
 class SubUpvote(View):
-
     def post(self, request, slug, pk, user, *args, **kwargs):
         sub = get_object_or_404(Sub, pk=pk)
-        # @loginrequired
         if request.user.username == user:
             return HttpResponseRedirect(reverse('prompt_detail', args=[slug]))
         else:
             if sub.upvotes.filter(id=request.user.id).exists():
                 sub.upvotes.remove(request.user)
                 # voted = True
-                return HttpResponseRedirect(reverse('prompt_detail', args=[slug]))
+                return HttpResponseRedirect(reverse('prompt_detail',
+                                                    args=[slug]))
             else:
                 sub.upvotes.add(request.user)
                 # voted = False
-                return HttpResponseRedirect(reverse('prompt_detail', args=[slug]))
+                return HttpResponseRedirect(reverse('prompt_detail',
+                                                    args=[slug]))
